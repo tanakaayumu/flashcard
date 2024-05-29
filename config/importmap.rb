a8d5ee2e0e5672a8fc55e1fd1ac5aa76026d1277
a t <at@enoMacBook-Air.local>
2024-05-29 16:24:19 +0900

    modified importmap.rb

--- config/importmap.rb
+++ config/importmap.rb
@@ -1,7 +1,7 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application"
-pin "@hotwired/turbo-rails", to: "turbo.min.js"
-pin "@hotwired/stimulus", to: "stimulus.min.js"
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin "application", preload: true
+pin "@hotwired/turbo-rails", to: "https://ga.jspm.io/npm:@hotwired/turbo-rails@7.1.0/dist/turbo.min.js", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@2.0.0/dist/stimulus.min.js", preload: true
+pin "@hotwired/stimulus-loading", to: "https://ga.jspm.io/npm:@hotwired/stimulus-loading@1.0.0/dist/stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,20 +13,21 @@ part1 = Category.create(name: 'part1')
 
 # 画像フォルダの内容をもとにフラッシュカードを作成
 flashcards_data = [
-  { content: '城', image: 'flashcards/castle.jpg', audio: 'castle.mp3', category: part1 },
-  { content: '猫', image: 'flashcards/cat.jpg', audio: 'cat.mp3', category: part1 },
-  { content: '犬', image: 'flashcards/dog.jpg', audio: 'dog.mp3', category: part1 },
-  { content: '妖精', image: 'flashcards/fairy.jpg', audio: 'fairy.mp3', category: part1 },
-  { content: '山', image: 'flashcards/mountain.jpg', audio: 'mountain.mp3', category: part1 },
-  { content: '夜', image: 'flashcards/night.jpg', audio: 'night.mp3', category: part1 },
-  { content: 'ロボット', image: 'flashcards/robot.jpg', audio: 'robot.mp3', category: part1 },
-  { content: '侍', image: 'flashcards/samurai.jpg', audio: 'samurai.mp3', category: part1 },
-  { content: '船', image: 'flashcards/ship.jpg', audio: 'ship.mp3', category: part1 },
-  { content: '宇宙船', image: 'flashcards/spaceship.jpg', audio: 'spaceship.mp3', category: part1 },
-  { content: '夕日', image: 'flashcards/sunset.jpg', audio: 'sunset.mp3', category: part1 },
-  { content: '魔法使い', image: 'flashcards/wizard.jpg', audio: 'wizard.mp3', category: part1 }
+  { content: '城', image: 'castle.jpg', audio: 'castle.mp3', category: part1 },
+  { content: '猫', image: 'cat.jpg', audio: 'cat.mp3', category: part1 },
+  { content: '犬', image: 'dog.jpg', audio: 'dog.mp3', category: part1 },
+  { content: '妖精', image: 'fairy.jpg', audio: 'fairy.mp3', category: part1 },
+  { content: '山', image: 'mountain.jpg', audio: 'mountain.mp3', category: part1 },
+  { content: '夜', image: 'night.jpg', audio: 'night.mp3', category: part1 },
+  { content: 'ロボット', image: 'robot.jpg', audio: 'robot.mp3', category: part1 },
+  { content: '侍', image: 'samurai.jpg', audio: 'samurai.mp3', category: part1 },
+  { content: '船', image: 'ship.jpg', audio: 'ship.mp3', category: part1 },
+  { content: '宇宙船', image: 'spaceship.jpg', audio: 'spaceship.mp3', category: part1 },
+  { content: '夕日', image: 'sunset.jpg', audio: 'sunset.mp3', category: part1 },
+  { content: '魔法使い', image: 'wizard.jpg', audio: 'wizard.mp3', category: part1 }
 ]
 
 flashcards_data.each do |data|
-Flashcard.find_or_create_by(content: data[:content], image: data[:image], audio: data[:audio], category: data[:category])
+  Flashcard.find_or_create_by(content: data[:content], image: data[:image], audio: data[:audio], category: data[:category])
 end
+
